70b594f2758079600c52bbe9c457909f0eea0a31
km3r <kylem3r@gmail.com>
2017-05-11 15:40:45 -0700

    adding server test frame

---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * (Short Description)
+ *
+ * @author Kyle Rosenthal
+ * @version (AssignmentName)
+ */
+public class ServerTest {
+
+   @Test
+   public void TestServer() {
+
+   }
+
+}
